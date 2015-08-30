7f7c1a3c5fe61eff3d62cb714be67b040ba6e8ff
budioktaviyan <budioktaviyans@gmail.com>
2015-08-30 23:56:54 +0700

    Add text statement after click like or dislike button (next change to image statement)

--- src/main/java/id/sikerang/mobile/activity/ProductActivity.java
+++ src/main/java/id/sikerang/mobile/activity/ProductActivity.java
@@ -5,7 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import butterknife.Bind;
@@ -16,20 +18,29 @@ import id.sikerang.mobile.SiKerang;
 /**
  * @author Budi Oktaviyan Suryanto (budioktaviyans@gmail.com)
  */
-public class ProductActivity extends AppCompatActivity {
+public class ProductActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = ProductActivity.class.getSimpleName();
 
     @Bind(R.id.toolbar_top)
     Toolbar mToolbarTop;
 
     @Bind(R.id.tv_product)
-    TextView mTextView;
+    TextView mTextViewProduct;
+
+    @Bind(R.id.tv_statement)
+    TextView mTextViewStatment;
 
     @Bind(R.id.btn_price)
     Button mButtonPrice;
 
     @Bind(R.id.btn_location)
     Button mButtonLocation;
+
+    @Bind(R.id.btn_likes)
+    ImageButton mImageButtonLikes;
+
+    @Bind(R.id.btn_dislikes)
+    ImageButton mImageButtonDislikes;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -46,11 +57,29 @@ public class ProductActivity extends AppCompatActivity {
         return true;
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_likes: {
+                mTextViewStatment.setTextColor(SiKerang.getContext().getResources().getColor(R.color.teal_500));
+                mTextViewStatment.setText(SiKerang.getContext().getResources().getString(R.string.text_likes));
+                break;
+            }
+            case R.id.btn_dislikes: {
+                mTextViewStatment.setTextColor(SiKerang.getContext().getResources().getColor(R.color.red_500));
+                mTextViewStatment.setText(SiKerang.getContext().getResources().getString(R.string.text_dislikes));
+                break;
+            }
+        }
+    }
+
     private void initComponents() {
         mToolbarTop.setTitle(SiKerang.getContext().getResources().getString(R.string.app_desc));
         setSupportActionBar(mToolbarTop);
-        mTextView.setText(SiKerang.getContext().getResources().getString(R.string.dummy_sugar));
+        mTextViewProduct.setText(SiKerang.getContext().getResources().getString(R.string.dummy_sugar));
         mButtonPrice.setText(SiKerang.getContext().getResources().getString(R.string.dummy_price));
         mButtonLocation.setText(SiKerang.getContext().getResources().getString(R.string.dummy_location));
+        mImageButtonLikes.setOnClickListener(this);
+        mImageButtonDislikes.setOnClickListener(this);
     }
 }
