e50b3981ed45aad4576a3fdb23b263427e64105b
budioktaviyan <budioktaviyans@gmail.com>
2015-08-30 17:50:23 +0700

    Change product screen layout

--- src/main/java/id/sikerang/mobile/SiKerang.java
+++ src/main/java/id/sikerang/mobile/SiKerang.java
@@ -1,9 +1,25 @@
 package id.sikerang.mobile;
 
 import android.app.Application;
+import android.content.Context;
+import android.util.Log;
 
 /**
  * @author Budi Oktaviyan Suryanto (budioktaviyans@gmail.com)
  */
 public class SiKerang extends Application {
+    private static final String TAG = SiKerang.class.getSimpleName();
+
+    private static Context CONTEXT;
+
+    public static Context getContext() {
+        return CONTEXT;
+    }
+
+    @Override
+    public void onCreate() {
+        Log.d(TAG, "onCreate SiKerang.");
+        super.onCreate();
+        CONTEXT = getApplicationContext();
+    }
 }
--- src/main/java/id/sikerang/mobile/activity/ProductActivity.java
+++ src/main/java/id/sikerang/mobile/activity/ProductActivity.java
@@ -2,25 +2,41 @@ package id.sikerang.mobile.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
+import android.view.Menu;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import id.sikerang.mobile.R;
+import id.sikerang.mobile.SiKerang;
 
 /**
  * @author Budi Oktaviyan Suryanto (budioktaviyans@gmail.com)
  */
-public class ProductActivity extends AppCompatActivity implements View.OnClickListener {
+public class ProductActivity extends AppCompatActivity {
     private static final String TAG = ProductActivity.class.getSimpleName();
+
+    @Bind(R.id.toolbar_top)
+    Toolbar mToolbarTop;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         Log.d(TAG, "onCreate ProductActivity");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product);
+        ButterKnife.bind(this);
+        initComponents();
     }
 
     @Override
-    public void onClick(View v) {
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    private void initComponents() {
+        mToolbarTop.setTitle(SiKerang.getContext().getResources().getString(R.string.app_desc));
+        setSupportActionBar(mToolbarTop);
     }
 }
