a7840ce8d7697a63b116c1e7fcc8c59d4d100468
Fanny Irawan Sutawanir <fanny@bizzy.co.id>
2015-09-29 02:09:29 +0700

    Implements slide up edit text to input Curhat

--- src/main/java/id/sikerang/mobile/fragment/KomoditasFragment.java
+++ src/main/java/id/sikerang/mobile/fragment/KomoditasFragment.java
@@ -1,16 +1,27 @@
 package id.sikerang.mobile.fragment;
 
+import android.content.Context;
+import android.graphics.Rect;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.view.WindowManager;
+import android.view.animation.TranslateAnimation;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import com.viewpagerindicator.CirclePageIndicator;
 
@@ -36,10 +47,17 @@ public class KomoditasFragment extends Fragment implements View.OnClickListener,
     @Bind(R.id.fab_mahal)
     FloatingActionButton mFabMahal;
 
+    @Bind(R.id.ll_curhat)
+    LinearLayout mLlCurhat;
+
     @Bind(R.id.btn_curhat)
     Button mButtonCurhat;
 
+    @Bind(R.id.et_curhat)
+    EditText mEtCurhat;
+
     private KomoditasAdapter mKomoditasAdapter;
+    private boolean mIsEtCurhatExpanded;
 
     @Nullable
     @Override
@@ -48,6 +66,35 @@ public class KomoditasFragment extends Fragment implements View.OnClickListener,
         ButterKnife.bind(this, view);
         initComponents();
         initAdapters();
+
+        final View activityRootView = getActivity().findViewById(android.R.id.content);
+        activityRootView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+
+            private boolean wasOpened;
+            private final int DefaultKeyboardDP = 100;
+            private final int EstimatedKeyboardDP = DefaultKeyboardDP + (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP ? 48 : 0);
+            private final Rect r = new Rect();
+
+            @Override
+            public void onGlobalLayout() {
+                // Convert the dp to pixels.
+                int estimatedKeyboardHeight = (int) TypedValue
+                        .applyDimension(TypedValue.COMPLEX_UNIT_DIP, EstimatedKeyboardDP, activityRootView.getResources().getDisplayMetrics());
+
+                // Conclude whether the keyboard is shown or not.
+                activityRootView.getWindowVisibleDisplayFrame(r);
+                int heightDiff = activityRootView.getRootView().getHeight() - (r.bottom - r.top);
+                boolean isShown = heightDiff >= estimatedKeyboardHeight;
+                if(isShown) {
+                    mFabMurah.hide();
+                    mFabMahal.hide();
+                }
+                else {
+                    mFabMurah.show();
+                    mFabMahal.show();
+                }
+            }
+        });
 
         return view;
     }
@@ -60,8 +107,41 @@ public class KomoditasFragment extends Fragment implements View.OnClickListener,
                 mKomoditasAdapter.onClick(view);
                 break;
             }
-            case R.id.btn_curhat: {
-                initDialogFragments();
+            case R.id.btn_curhat:
+            case R.id.ll_curhat: {
+                if(!mIsEtCurhatExpanded) {
+                    //TranslateAnimation slide = new TranslateAnimation(0, 0, 300, 0);
+                    //slide.setDuration(200);
+                    //slide.setFillAfter(true);
+                    //mEtCurhat.startAnimation(slide);
+                    mEtCurhat.setVisibility(View.VISIBLE);
+                    mEtCurhat.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            mEtCurhat.requestFocus();
+                            InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+                            imm.showSoftInput(mEtCurhat, InputMethodManager.SHOW_IMPLICIT);
+                        }
+                    }, 100);
+                    mIsEtCurhatExpanded = true;
+                    mFabMurah.hide();
+                    mFabMahal.hide();
+                }
+                else {
+                    //TranslateAnimation slide = new TranslateAnimation(0, 0, 0, 300);
+                    //slide.setDuration(200);
+                    //slide.setFillAfter(true);
+                    //mEtCurhat.startAnimation(slide);
+                    mEtCurhat.setVisibility(View.GONE);
+                    mIsEtCurhatExpanded = false;
+                    View focusView = getActivity().getCurrentFocus();
+                    if (focusView != null) {
+                        InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+                        imm.hideSoftInputFromWindow(focusView.getWindowToken(), 0);
+                    }
+                    mFabMurah.show();
+                    mFabMahal.show();
+                }
                 break;
             }
         }
@@ -77,7 +157,9 @@ public class KomoditasFragment extends Fragment implements View.OnClickListener,
         getActionBar().setTitle(title);
         mFabMurah.setOnClickListener(this);
         mFabMahal.setOnClickListener(this);
+        mLlCurhat.setOnClickListener(this);
         mButtonCurhat.setOnClickListener(this);
+        mIsEtCurhatExpanded = false;
     }
 
     private void initAdapters() {
