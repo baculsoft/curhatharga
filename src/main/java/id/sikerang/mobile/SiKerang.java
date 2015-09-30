9a4db8f2e9f67bb5ba49ef67acb02616520f7b93
budioktaviyan <budioktaviyans@gmail.com>
2015-09-30 09:51:19 +0700

    Cleanup

--- src/main/java/id/sikerang/mobile/SiKerang.java
+++ src/main/java/id/sikerang/mobile/SiKerang.java
@@ -2,24 +2,19 @@ package id.sikerang.mobile;
 
 import android.app.Application;
 import android.content.Context;
-import android.util.Log;
 
 /**
  * @author Budi Oktaviyan Suryanto (budioktaviyans@gmail.com)
  */
 public class SiKerang extends Application {
-    private static final String TAG = SiKerang.class.getSimpleName();
-
     static volatile Context CONTEXT;
 
     public static synchronized Context getContext() {
-        Log.d(TAG, "Synch SiKerang.getContext()");
         return CONTEXT;
     }
 
     @Override
     public void onCreate() {
-        Log.d(TAG, "onCreate SiKerang.");
         super.onCreate();
         CONTEXT = getApplicationContext();
     }
